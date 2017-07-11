906c0e02d0bde826f20b25abe73e49bc62d21b39
fei.wang <fei.wang@ushow.media>
2017-07-11 23:05:03 +0800

    fix了git兼容性问题

--- src/scripts/build_proxy.sh
+++ src/scripts/build_proxy.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-go build -ldflags "-X main.buildDate=`date +%Y%m%d%H%M%S` -X main.gitVersion=`git -C .. rev-parse HEAD`" cmds/service_rpc_proxy.go
+go build -ldflags "-X main.buildDate=`date +%Y%m%d%H%M%S` -X main.gitVersion=`git rev-parse HEAD`" cmds/service_rpc_proxy.go
